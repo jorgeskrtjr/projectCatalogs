26944b5474dfc340160a98405da615512673e880
Jorge jr <jorgesilvajunior10@gmail.com>
2023-01-25 15:25:47 -0300

    add: clear react project and add axios

--- frontend/frontcatalog/src/App.js
+++ frontend/frontcatalog/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './utils/request'
+import { url } from './utils/request';
+import axios from "axios"; 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+  function findAll(){
+
+  }
+  
+  return(
+     console.log(findAll())
   );
 }
 
